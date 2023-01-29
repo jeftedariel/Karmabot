ca475887e915a2c465cb333f5b56510df283a7ef
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2023-01-29 00:17:58 -0600

    Sistema de moderacion version 1.1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
+const blacklisted = require('./blacklist.json');
 //const mcping = require('mcping-js')
 //const server = new mcping.MinecraftServer('', 25565)
 const { Client, Collection, Events, GatewayIntentBits, ActivityType, AuditLogEvent, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, embedLength, EmbedBuilder, ButtonBuilder, ButtonStyle, Message, StringSelectMenuBuilder  } = require('discord.js');
@@ -9,6 +10,7 @@ const { colors, yellow } = require('colors');
 const { channel } = require('node:diagnostics_channel');
 const { Console } = require('node:console');
 const { MessageChannel } = require('node:worker_threads');
+
 dotenv.config();
 require('./');
 
@@ -94,7 +96,7 @@ client.on('messageCreate', (message) => {
 	const Aviso = new EmbedBuilder()
 		.setColor(0x0099FF)
 		.setTitle('Aviso')
-		.setDescription('Su mensaje se eliminó debido a que incluia lenguaje inadecuado, ayudanos a mantener un ambiente agradable.')
+		.setDescription('Su mensaje se eliminó debido a que incluia lenguaje o hablaba sobre temas inadecuados o que no están permitidos este servidor, ayudanos a mantener un ambiente agradable. Para más información o consultas puedes abrir un ticket')
 		.setImage('https://media.giphy.com/media/7VbE2HvYu1QUmoVQlt/giphy.gif')
 		.setFooter({ text: 'Karmafans', iconURL: 'https://cdn.discordapp.com/attachments/1065028049877348382/1065717118974316615/karmaland.png' });
 
@@ -111,42 +113,17 @@ client.on('messageCreate', (message) => {
 //	}
 
 
-	if (/\bbasura\b/i.test(message.content)) {
+	let check = false
+	for (var palabra in blacklisted) {
+		if (message.content.toLowerCase().includes(blacklisted[palabra].toLowerCase())) check = true
+	}
+	if (check) {
 		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
 		setTimeout(() => message.delete(), 1);
 		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	if (/\bestupido\b/i.test(message.content)) {
-		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
-		setTimeout(() => message.delete(), 1);
-		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	if (/\bmierda\b/i.test(message.content)) {
-		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
-		setTimeout(() => message.delete(), 1);
-		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	if (/\bputa\b/i.test(message.content)) {
-		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
-		setTimeout(() => message.delete(), 1);
-		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	if (/\bidiota\b/i.test(message.content)) {
-		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
-		setTimeout(() => message.delete(), 1);
-		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	if (/\bimbecil\b/i.test(message.content)) {
-		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
-		setTimeout(() => message.delete(), 1);
-		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	if (/\bretrasado\b/i.test(message.content)) {
-		console.log('[', '!'.green, ']', 'Se eliminó un mensaje del usuario', message.author.username, 'el cual contenia:', message.content)
-		setTimeout(() => message.delete(), 1);
-		message.author.send({ embeds: [Aviso] }).catch(console.error)
-	}
-	
+	};
+
+
 }),
 
 
