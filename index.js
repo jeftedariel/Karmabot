b890d88aee33655d17099b057b4be8ba7f4ab948
Jefte <jeftedariel3@gmail.com>
2023-01-31 12:03:29 -0600

    fix

--- index.js
+++ index.js
@@ -151,9 +151,9 @@ client.on("messageCreate", message => {
 		)
 	if (/7346sdaksasdsbggedaiub52536/i.test(message.content)) {
 		const channel = client.channels.cache.find(channel => channel.id === "1058921412632518748")
-		//channel.send({ embeds: [Anuncio] });
-		//channel.send('@everyone');
-		message.author.send({ embeds: [Anuncio] }).catch(console.error)
+		channel.send({ embeds: [Anuncio] });
+		channel.send('@everyone');
+		//message.author.send({ embeds: [Anuncio] }).catch(console.error)
 		message.delete()
 		console.log('[', '!'.green, ']', 'Anuncio enviado exitosamente por', message.author.username)
 		const log = client.channels.cache.find(channel => channel.id === "1069336879968813158")
