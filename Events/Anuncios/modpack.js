2a9c3bf96b70fece4cb6a28efdc5a686d95150c8
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2023-03-21 18:11:34 -0600

    modlink

--- Events/Anuncios/modpack.js
+++ Events/Anuncios/modpack.js
@@ -13,7 +13,7 @@ module.exports = {
 
             const Anuncio = new EmbedBuilder()
                 .setColor(0x0099FF)
-                .setTitle('IP y Modpack Java 1.18 (SERVIDOR TEMPORAL)')
+                .setTitle('IP y Modpack Java 1.18 \n \n _(SERVIDOR TEMPORAL)_')
                 .setDescription('```temp.karmafans.xyz:40049```')
                 .setImage('https://cdn.discordapp.com/attachments/1065028049877348382/1072349024704806932/rgb.gif')
             const Anuncioboton = new ActionRowBuilder()
@@ -25,8 +25,16 @@ module.exports = {
                 ) 
             if (/assddaszxxxxdddcvzxxnopiqn3d45f/i.test(message.content)) {
                 const channel = client.channels.cache.find(channel => channel.id === "1074320286079533188")
-                const channeltest = client.channels.cache.find(channel => channel.id === "1065028049877348382")
                 channel.send({ embeds: [Anuncio], components: [Anuncioboton] });
+                message.delete()
+                console.log('[', '!'.green, ']', 'Anuncio enviado exitosamente por', message.author.username)
+                const log = client.channels.cache.find(channel => channel.id === "1069336879968813158")
+                log.send('[ ! ] ' + 'Anuncio enviado exitosamente por ' + message.author.username);
+    
+            }
+            if (/assddasasdasdasdasdaszxxxxdddc2dasdaxzvzxxnopiqn3d45f/i.test(message.content)) {
+                const channeltest = client.channels.cache.find(channel => channel.id === "1065028049877348382")
+                channeltest.send({ embeds: [Anuncio], components: [Anuncioboton] });
                 message.delete()
                 console.log('[', '!'.green, ']', 'Anuncio enviado exitosamente por', message.author.username)
                 const log = client.channels.cache.find(channel => channel.id === "1069336879968813158")
