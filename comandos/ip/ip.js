51186757d62fd5a5f05d7dbda7bcd54fccfeefca
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2023-07-04 23:46:15 -0600

    ip

---
+++ comandos/ip/ip.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require('@discordjs/builders');
+const {SlashCommandBuilder, Embedbuilder, PermissionFlagsBits, Embed} = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ip')
+    .setDescription('Ip Karmafans.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+    
+        async execute(interaction) {
+            const {channel} = interaction;
+
+            const embed = new EmbedBuilder()
+                .setDescription('IP: \n ```mc.karmafans.xyz``` \n Puerto BedrocK: \n ```25565``` \n Version: \n ```1.16.5 - 1.20.x')
+                .setColor('Random')
+                .setTimestamp()
+                .setFooter({ text: 'Karmafans', iconURL: 'https://cdn.discordapp.com/attachments/1065028049877348382/1065717118974316615/karmaland.png' });
+
+            await interaction.reply({ embeds:[embed] });
+        }   
+}
